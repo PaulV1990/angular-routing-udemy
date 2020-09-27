421578410ae308af1ee26743e4f5e39fb9920488
Paul <paul.vervuurt@student.fontys.nl>
2020-09-27 20:41:16 +0200

    Reactive route parameters

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -13,10 +13,17 @@ export class UserComponent implements OnInit {
   }
 
   ngOnInit() {
+    // No other way than reaching this with an Init, use snapshot!
     this.user = {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
+    // Params is an Observable and snapshot only used one time. So changing or other ways of loading it from the inside of this component
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.user.id = params['id'];
+        this.user.name = params['name'];
+      }
+    );
   }
-
 }
